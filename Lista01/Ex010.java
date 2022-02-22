6533c07c4b5c4ac4568f04e15f0e98632801d7ec
FelipeElias021 <21felipe.elias@gmail.com>
2022-02-22 19:58:21 -0300

    Ex 10 e 11 - Lista 01

--- Lista01/Ex010.java
+++ Lista01/Ex010.java
@@ -3,11 +3,22 @@ package Lista01;
 import java.util.Scanner;
 
 /* Faça um algoritmo que leia x e y e resolva a equação abaixo:
-NAO CONSIGO VER AS EQUACÇÃO DO 10 E 11
+Eq = 2 * (V3x+30/3) + (y-32)^4
 */
 
 public class Ex010 {
     public static void main(String[] args) {
-        
+        double x, y, resultado;
+
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.print("X: ");
+            x = entrada.nextDouble();
+            System.out.print("Y: ");
+            y = entrada.nextDouble();
+        }
+
+        resultado = (2 * (Math.sqrt((3 * x) + 30)/3)) + (Math.pow(y-32, 4));
+
+        System.out.println("Resultado: " + resultado);
     }
 }
---
+++ Lista01/Ex011.java
@@ -0,0 +1,30 @@
+package Lista01;
+
+import java.util.Scanner;
+
+/* Dados os pontos A e B, cujas coordenadas reais A(x1, y1) e B(x2, y2) serão informadas  
+via teclado, escreva um programa que  calcule a distância euclidiana entre A e B, onde: :
+DistanciaEuclidiana = V(y2 - y1)^2 + (x2 - x1)^2
+*/
+
+public class Ex011 {
+    public static void main(String[] args) {
+        int x1, y1, x2, y2;
+
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.print("X do ponto A: ");
+            x1 = entrada.nextInt();
+            System.out.print("Y do ponto A: ");
+            y1 = entrada.nextInt();
+            System.out.print("X do ponto B: ");
+            x2 = entrada.nextInt();
+            System.out.print("Y do ponto B: ");
+            y2 = entrada.nextInt();
+        }
+
+        //distanciaRaiz = Math.pow(y2-y1, 2) + Math.pow(x2-x1, 2);
+        double distancia = Math.sqrt(Math.pow(y2-y1, 2) + Math.pow(x2-x1, 2));
+
+        System.out.println("Distância: " + distancia);
+    }
+}
